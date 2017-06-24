585748bd65cffc1dba0176e9e4354ae8fb49a353
seb45tian <sebastian.potthoff@gmail.com>
2017-06-24 19:33:01 +0200

    initial make_symlinks script

--- make_symlinks.sh
+++ make_symlinks.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/bash
 
-
+# FIRST UPDATE THE VIM SUBMODULE
+git submodule update --init --recursive
 
 ln -s bash_profile ../.bash_profile
 ln -s dircolors ../.dircolors
@@ -9,5 +10,7 @@ ln -s zshrc ../.zshrc
 
 
 
-cd vim
+cd vimrc/
+git submodule update --init --recursive
+cd ..
 ln -s vim/vimrc ../.vimrc
